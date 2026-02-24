d7d7e7ea26ead5c73636e66d0a5f0dc57148e678
Tomer Shlasky <tomer@decart.ai>
2026-02-24 15:10:05 +0200

    Fix modal shrinking before camera loads with aspect ratio

--- examples/ecommerce/components/TryOnModal.tsx
+++ examples/ecommerce/components/TryOnModal.tsx
@@ -125,7 +125,7 @@ export function TryOnModal({ product, onClose }: TryOnModalProps) {
           </button>
         </div>
 
-        <div className="relative flex-1 min-h-0 bg-gray-900">
+        <div className="relative flex-1 min-h-0 aspect-[4/3] bg-gray-900">
           <video
             ref={videoRef}
             autoPlay
